ab1918a1b670dbf08fbff5db06563479c9745e1d
Anil <abasdeo29@gmail.com>
2024-02-24 20:37:13 -0500

    finished tutorial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="home">
-    <div class="count">
-      {{ storeCounter.count }}
-    </div>
+    <Counter />
     <div class="buttons">
       <button @click="storeCounter.decreaseCount">-</button>
       <button @click="storeCounter.increaseCount">+</button>
@@ -11,10 +9,16 @@
     <div>
       This counter is: {{ storeCounter.oddOrEven }}
     </div>
+    <hr>
+    <div>
+      <h3>Edit counter:</h3>
+      <input type="number" v-model="storeCounter.count">
+    </div>
   </div>
 </template>
 
 <script setup>
+import Counter from '@/components/Counter.vue';
 import { useCounterStore } from '@/stores/counter';
 
 const storeCounter = useCounterStore()
